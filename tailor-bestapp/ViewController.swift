70564f92f2f12d53f6fe639980fc9302a6a1189a
precious dike <preamzydike22@gmail.com>
2021-03-11 14:28:38 +0100

    Initial Commit

---
+++ tailor-bestapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tailor-bestapp
+//
+//  Created by precious dike on 11/03/2021.
+//  Copyright © 2021 precious dike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
